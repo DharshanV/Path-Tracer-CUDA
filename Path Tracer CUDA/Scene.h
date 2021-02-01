7e68a0c8e054bea071190a5955d788e99411a26c
DharshanV <ddvyukjng7@gmail.com>
2021-02-01 01:31:37 -0800

    Updated scene hit

--- Path Tracer CUDA/Scene.h
+++ Path Tracer CUDA/Scene.h
@@ -8,16 +8,17 @@ public:
 
 	__device__
 	bool hit(const Ray& ray,vec3& hitPoint,vec3& N,Material& mat) {
-		float t;
+		float maxDistance = FLT_MAX;
 		for (int i = 0; i < objectSize; i++) {
-			if (objects[i]->rayIntersect(ray.origin,ray.dir,t)) {
+			float t;
+			if (objects[i]->rayIntersect(ray.origin,ray.dir,t) && t < maxDistance) {
 				hitPoint = ray.origin + ray.dir * t;
-				N = objects[i]->getNormal(hitPoint);
+				N = objects[i]->getNormal(hitPoint).normalised();
 				mat = *objects[i]->getMaterial();
-				return true;
+				maxDistance = t;
 			}
 		}
-		return false;
+		return maxDistance < 10000.0f;
 	}
 
 	__device__
