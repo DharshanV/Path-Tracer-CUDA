6b7450b934607ce0118dfee3cd76d0f9602de331
DharshanV <ddvyukjng7@gmail.com>
2021-02-02 19:30:51 -0800

    Updated render settings

--- Path Tracer CUDA/Settings.h
+++ Path Tracer CUDA/Settings.h
@@ -1,3 +1,3 @@
 #pragma once
-constexpr auto MAX_SAMPLES = 10;
+constexpr auto MAX_SAMPLES = 200;
 constexpr auto MAX_DEPTH = 50;
--- Path Tracer CUDA/main.cu
+++ Path Tracer CUDA/main.cu
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Renderer.h"
-#include <string>
+
 using namespace std;
 using namespace glm;
 
